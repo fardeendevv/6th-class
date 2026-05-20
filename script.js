d1b54624351d7118bf00b4fba49d74846d2b1457
fardeendevv <fardeenfarrukh1@gmail.com>
2026-05-20 14:55:10 -0700

    let

---
+++ script.js
@@ -0,0 +1,87 @@
+
+console.log("hello fardeen")
+
+let obtMarks = 858
+let totalMarks =1100
+let percentage= obtMarks/totalMarks*100
+
+
+
+console.log(percentage)
+
+
+// numbers
+let weight = 70
+let height = 1.75
+
+let bmi = weight/height**2
+
+
+
+console.log(bmi)
+
+//string
+let myName = "fardeen"
+ myName = "fardeen sheikh"
+console.log(myName)
+
+//boolean
+let iRainyDay = true
+
+console.log(iRainyDay)
+
+// conditional statement
+
+let age = 20
+
+if(age>=18){
+    alert("You are eligible to vote.")
+};
+
+
+let age = 16
+
+if(age>=18){
+    alert("You are eligible to vote.")
+}else{
+    alert("You are not eligible to vote.")
+};
+
+
+let numberOfChildren = 3
+
+if(numberOfChildren === 0){
+    console.log("You have no children.")
+}else if(numberOfChildren === 1){
+    console.log("You have one child.")
+}else if(numberOfChildren === 2){
+    console.log("You have two children.")
+}else{
+    console.log("You have more than two children.")
+}
+
+console.log(numberOfChildren)
+
+let gradepercentage = 80
+
+if(gradepercentage >= 90){
+    console.log("you are eligible for a scholarship")
+}
+else if(gradepercentage >= 80){
+    console.log("you are eligible for a scholarship")
+}
+else{
+    console.log("you are not eligible for a scholarship")
+};
+
+let animalWeight = 150
+
+if(animalWeight > 100){
+    console.log("The animal is heavy.")
+}
+else if(animalWeight > 50){
+    console.log("The animal is medium weight.")
+}
+else{
+    console.log("The animal is light.")
+};
